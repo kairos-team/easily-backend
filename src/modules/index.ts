dc84e35899ee080b3bcdc5746f5b578b61e8d815
Rafael Landim <rafaelmlandim@hotmail.com>
2026-03-24 16:56:08 -0300

    Changes to the routes in the  index.js file.

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,17 +2,23 @@ import { Router } from "express";
 import clientRoutes from "./clients/client.routes";
 import authRoutes from "./auth/auth.routes";
 import userRoutes from "./users/user.routes";
+import companyRoutes from "./companies/company.routes";
+import productRoutes from "./products/product.routes";
+import serviceOrderRoutes from "./service-orders/service-order.routes";
+import stocksRoutes from "./stocks/stock.routes";
+import suppliersRoutes from "./suppliers/supplier.routes";
+import vehiclesRoutes from "./vehicles/vehicle.routes";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/clients", clientRoutes);
-router.use("/companies", authRoutes);
-router.use("/products", authRoutes);
-router.use("/service-orders", authRoutes);
-router.use("/stocks", authRoutes);
-router.use("/suppliers", authRoutes);
+router.use("/companies", companyRoutes);
+router.use("/products", productRoutes);
+router.use("/service-orders", serviceOrderRoutes);
+router.use("/stocks", stocksRoutes);
+router.use("/suppliers", suppliersRoutes);
 router.use("/users", userRoutes);
-router.use("/vehicles", authRoutes);
+router.use("/vehicles", vehiclesRoutes);
 
 export default router;
