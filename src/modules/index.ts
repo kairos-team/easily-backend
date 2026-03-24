c6b555931c1511ddc3f373408ef90e4ddbe4ba5b
Rafael Landim <rafaelmlandim@hotmail.com>
2026-03-24 16:21:43 -0300

    route file change

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,17 +1,18 @@
 import { Router } from "express";
-import clientRoutes from "./clients/client.routes"
-import authRoutes from "./auth/auth.routes"
+import clientRoutes from "./clients/client.routes";
+import authRoutes from "./auth/auth.routes";
+import userRoutes from "./users/user.routes";
 
 const router = Router();
 
-router.use("/auth", authRoutes)
+router.use("/auth", authRoutes);
 router.use("/clients", clientRoutes);
-router.use("/companies", authRoutes)
-router.use("/products", authRoutes)
-router.use("/service-orders", authRoutes)
-router.use("/stocks", authRoutes)
-router.use("/suppliers", authRoutes)
-router.use("/users", authRoutes)
-router.use("/vehicles", authRoutes)
+router.use("/companies", authRoutes);
+router.use("/products", authRoutes);
+router.use("/service-orders", authRoutes);
+router.use("/stocks", authRoutes);
+router.use("/suppliers", authRoutes);
+router.use("/users", userRoutes);
+router.use("/vehicles", authRoutes);
 
 export default router;
